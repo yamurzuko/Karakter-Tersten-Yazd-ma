d6bf4fa88e62c066e12e8d5f03c8b13f4301d128
yamurzuko <yamurzcan@gmail.com>
2022-10-14 02:47:22 +0300

    Result

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace KarakterTerstenYazdirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Merhaba Hello Question";
+            string newSentence = "";
+            string firstLetter = "";
+            for (int i = 0; i <= sentence.Length; i++)
+            {
+                if (i == 0)
+                {
+                    firstLetter = sentence[i].ToString();
+                }
+                else if (i == sentence.Length)
+                {
+                    newSentence += firstLetter;
+                    break;
+                }
+                else if (sentence[i].ToString() == " ")
+                {
+                    newSentence += firstLetter + " ";
+                    firstLetter = sentence[i+1].ToString();
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    newSentence += sentence[i].ToString();
+                }
+            }
+            Console.WriteLine(newSentence);
+        }
+    }
+}
